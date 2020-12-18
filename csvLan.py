068d3a24ad2b40a13bc280322a7ac623f25d3b6c
yizi <liuyizhi@hytto.com>
2020-12-18 15:40:57 +0800

    no message

--- csvLan.py
+++ csvLan.py
@@ -1,36 +1,60 @@
 import csv
-from os import read
+import os
+import re
 
-# {"en": {"{key}": "{content}"}, "ja": {"{key}": "{content}"}}
-with open('lan.csv', newline='') as csvfile:
-    reader = csv.DictReader(csvfile)
-    dict = {}
-    for name in reader.fieldnames:
-        if name == 'key':
-            continue
-        dict[name] = {}
 
-    for row in reader:
-        for key in dict.keys():
-            dict[key][row['key']] = row[key]
+# key 必须为 csv 文件第一列第一个元素
+# TODO 边读边写？减少内存使用
+#
+#
 
-    list = []
+
+def convertToSwift(dict, filename):
+    # dict 内容 {"en": {"{key}": "{content}"}, "ja": {"{key}": "{content}"}}
+    # 文件内容，list 为每行文字的数组
     for key in dict.keys():
-        list.append(key)
-        list.append('\n')
+        list = []
         items = dict[key]
         for item in items:
             s = '"{key}" = "{content}";'
-            list.append(s.format(key=item, content=items[item]))
+            # 替换变量
+            text = s.format(key=item, content=items[item])
+            # 将换行符转化为字符串
+            text = text.replace('\n', '\\n')
+            # 替换所有 [] 中括号包裹的内容为 %@
+            text = re.sub(r'\[\w+\]', '%@', text)
+            list.append(text)
             list.append('\n')
-        list.append('-------')
-        list.append('\n\n')
-
-    # content = ''.join(str(s) for s in list)
-    # print(content)
-
-    with open('output', 'w') as file:
-        file.write(''.join(str(s) for s in list))
-        file.close()
-        pass
+        outputname = '{file}-{lang}-output'.format(
+            file=filename, lang=key)
+        with open(outputname, 'w', newline='\n') as file:
+            file.write(''.join(str(s) for s in list))
+            file.close()
+            pass
     pass
+
+
+def readCSV(filepath):
+    # filepath csv文件的路径包括文件名后缀
+    with open(filepath, newline='\n') as csvfile:
+        (name, ext) = os.path.splitext(filepath)
+        print(os.path.splitext(filepath))
+        reader = csv.DictReader(csvfile)
+        # dict 的内容： {"en": {"{key}": "{content}"}, "ja": {"{key}": "{content}"}}
+        dict = {}
+        for key in reader.fieldnames:
+            if key == 'key':
+                continue
+            dict[key] = {}
+
+        for row in reader:
+            for key in dict.keys():
+                dict[key][row['key']] = row[key]
+
+        convertToSwift(dict, name)
+
+        return dict
+        pass
+
+
+readCSV('pin.csv')
